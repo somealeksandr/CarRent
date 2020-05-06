37a66aa78c37e8c1d8bd6bce4db4fe6b8fe80163
somealeksandr <omelkovets69@gmail.com>
2020-05-06 20:38:02 +0300

    jenkins

--- app/app/settings.py
+++ app/app/settings.py
@@ -138,8 +138,8 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'omelkovets69@gmail.com'
-EMAIL_HOST_PASSWORD = '47331213WAsd'
-EMAIL_USE_TLS = True
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = 'omelkovets69@gmail.com'
+# EMAIL_HOST_PASSWORD = '47331213WAsd'
+# EMAIL_USE_TLS = True
